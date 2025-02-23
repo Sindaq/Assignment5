09dc38151e57463ba082bc49570697a8f33e33e6
Sindaq <gabesindac1998@gmail.com>
2025-02-23 17:55:10 -0600

    initial commit

---
+++ src/test/java/org/example/Assignment/FilterInvoiceTest.java
@@ -0,0 +1,14 @@
+package org.example.Assignment;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilterInvoiceTest {
+
+    @Test
+    void filterInvoiceTest() {
+
+    }
+
+}

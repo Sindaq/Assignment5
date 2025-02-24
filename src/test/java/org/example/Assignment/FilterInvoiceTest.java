75c410221c851a87ad9df1409e8aeb8f18397ec5
Sindaq <gabesindac1998@gmail.com>
2025-02-23 20:07:17 -0600

    Gabe Sindac – Improve Testability and Stubbed Dependency

--- src/test/java/org/example/Assignment/FilterInvoiceTest.java
+++ src/test/java/org/example/Assignment/FilterInvoiceTest.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class FilterInvoiceTest {
 
@@ -13,7 +15,7 @@ class FilterInvoiceTest {
     void filterInvoiceTest() {
         Database db = new Database();
         QueryInvoicesDAO dao = new QueryInvoicesDAO(db);
-        FilterInvoice filterInvoice = new FilterInvoice();
+        FilterInvoice filterInvoice = new FilterInvoice(dao);
         Invoice i1 = new Invoice("Gabe", 99);
         Invoice i2 = new Invoice("Emelyn", 100);
 
@@ -24,6 +26,32 @@ class FilterInvoiceTest {
         dao.save(i2);
 
         assertEquals(listInvoice, filterInvoice.lowValueInvoices());
+    }
+
+    @Test
+    void filterInvoiceStubbedTest() {
+
+//        Database db = mock(Database.class);
+        QueryInvoicesDAO dao = mock(QueryInvoicesDAO.class);
+
+        Invoice i1 = new Invoice("Gabe", 99);
+        Invoice i2 = new Invoice("Emelyn", 100);
+
+        List<Invoice> listInvoice = new ArrayList<>();
+        List<Invoice> expected = new ArrayList<>();
+        listInvoice.add(i1);
+        listInvoice.add(i2);
+        expected.add(i1);
+
+        dao.save(i1);
+        dao.save(i2);
+
+        when(dao.all()).thenReturn(listInvoice);
+        FilterInvoice filterInvoice = new FilterInvoice(dao);
+
+        List<Invoice> mockList = filterInvoice.lowValueInvoices();
+
+        assertEquals(expected, mockList);
     }
 
 }

c740b273ea33a228fde94848deaef3bd0d1cd41a
Sindaq <gabesindac1998@gmail.com>
2025-02-23 19:39:25 -0600

    Gabriel Sindac – Integration Testing

--- src/test/java/org/example/Assignment/FilterInvoiceTest.java
+++ src/test/java/org/example/Assignment/FilterInvoiceTest.java
@@ -2,13 +2,28 @@ package org.example.Assignment;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FilterInvoiceTest {
 
     @Test
     void filterInvoiceTest() {
-        // integration testing
+        Database db = new Database();
+        QueryInvoicesDAO dao = new QueryInvoicesDAO(db);
+        FilterInvoice filterInvoice = new FilterInvoice();
+        Invoice i1 = new Invoice("Gabe", 99);
+        Invoice i2 = new Invoice("Emelyn", 100);
+
+        List<Invoice> listInvoice = new ArrayList<>();
+        listInvoice.add(i1);
+
+        dao.save(i1);
+        dao.save(i2);
+
+        assertEquals(listInvoice, filterInvoice.lowValueInvoices());
     }
 
 }
